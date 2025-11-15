196994c3054ff379125b8b300bbaac70d56996d8
Leslie Claussen <78480772+IamLeslieClaussen@users.noreply.github.com>
2025-11-15 14:29:26 +0000

    latest commit

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -11,21 +11,38 @@ const Navbar = () => {
            <span>lamaLog</span>
         </div>
         <div className='md:hidden'>
-            <div className='cursor-pointer text-4xl bg-green-600 '
+            <div className='cursor-pointer text-4xl'
             onClick={() => setOpen((prev) => !prev)}
             >
                 {open ? "X" : "☰"}
             </div>
             {/* MOBILE LINK LIST*/}
             <div
-            className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
-            open ? "-right-0" : "-right-[100%]"
+            className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? "right-0" : "-right-full"
           }`}
 
-        > </div>
+        > 
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className='py-2 px-4 rounded-3xl bg-blue-700 text-white'>Login</button>
+          </a>
+        
+        </div>
         </div>
         {/* DESKTOP MENU*/}
-        <div className='hidden md:flex'>D</div>
+        <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className='py-2 px-4 rounded-3xl bg-blue-700 text-white'>Login</button>
+          </a>
+        </div>
       </div>
       
 )}
