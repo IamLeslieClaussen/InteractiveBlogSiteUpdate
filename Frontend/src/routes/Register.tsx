8f3deaf3c39b46779e09a6f95fb56ab0ba1e13e2
Leslie Claussen <78480772+IamLeslieClaussen@users.noreply.github.com>
2025-11-21 14:07:34 +0000

    latest commit

--- Frontend/src/routes/Register.tsx
+++ Frontend/src/routes/Register.tsx
@@ -1,7 +1,13 @@
+import { SignUp } from "@clerk/clerk-react"
+
 const Register = () => {
     return (
-        <div className="">Register</div>
+        <div className="">
+            <div className="flex items-center justify-center h-[calc(100vh-80px)]"> 
+                <SignUp signInUrl="/login"/>
+            </div> 
+        </div> // <--- This was the missing closing </div> tag
     )
-}
+} // <--- This was the incorrectly positioned closing brace
 
 export default Register
